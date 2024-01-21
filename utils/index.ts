d9099eb010a4fcf18684ac969fa60cd7a0595981
Torjnilsen <torjus987@gmail.com>
2024-01-21 18:47:48 +0100

    message(url)

--- utils/index.ts
+++ utils/index.ts
@@ -37,15 +37,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const { make, model, year } = car;
   
     url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
-    url.searchParams.append('make', make);
+    url.searchParams.append('make', String(make)); // Convert make to string
     url.searchParams.append('modelFamily', model.split(" ")[0]);
     url.searchParams.append('zoomType', 'fullscreen');
     url.searchParams.append('modelYear', `${year}`);
-    // url.searchParams.append('zoomLevel', zoomLevel);
-    url.searchParams.append('angle', `${angle}`);
+  
+    // Check if angle is provided before appending
+    if (angle !== undefined) {
+      url.searchParams.append('angle', `${angle}`);
+    }
   
     return `${url}`;
-  } 
+  };
+  
 
   export const updateSearchParams = (type: string, value: string) => {
     const searchParams = new URLSearchParams(window.location.search);
